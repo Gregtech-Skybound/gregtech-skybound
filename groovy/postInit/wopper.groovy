632b73d315baa558f3d9b7a4dda66410c505d029
Irgendwer <wippichdpaul@gmail.com>
2025-11-25 22:36:41 +0100

    Actually have a chest in the middle in the recipe

--- groovy/postInit/wopper.groovy
+++ groovy/postInit/wopper.groovy
@@ -1,5 +1,5 @@
 crafting.removeByOutput(item('wopper:wopper'))
 crafting.addShaped('wopper', item('wopper:wopper'), [
  [ore('plankWood'), null, ore('plankWood')],
- [ore('plankWood'), null, ore('plankWood')],
+ [ore('plankWood'), item('minecraft:chest'), ore('plankWood')],
  [ore('craftingToolSaw'), ore('plankWood'), ore('craftingToolSoftHammer')]])
